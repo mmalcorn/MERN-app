a2681d63daa20fc0f839a0eb5f128f20985b5625
Meredith Alcorn <alcornmeredith@gmail.com>
2018-08-26 16:38:14 -0400

    Add MongoDB configuration and connect using mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://meredith:dbpass1@ds133632.mlab.com:33632/mernstackkk"
+};
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
 
 // process.env.port used for heroku deployment, or localhost:5000 for dev
 const port = process.env.PORT || 5000;
