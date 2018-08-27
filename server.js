9f110e507574728547e711e803027f6cc847f5ee
Meredith Alcorn <alcornmeredith@gmail.com>
2018-08-26 20:00:19 -0400

    Create endpoints for auth, posts, and profile using express router

--- server.js
+++ server.js
@@ -5,15 +5,23 @@ const app = express();
 // DB Config
 const db = require('./config/keys').mongoURI;
 
+const auth = require('./routes/api/auth');
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, {useNewUrlParser: true})
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
+
+  app.get('/', (req, res) => res.send('Hello'));
+  app.use('/api/auth', auth);
+  app.use('/api/profile', profile);
+  app.use('/api/posts', posts);
 
 // process.env.port used for heroku deployment, or localhost:5000 for dev
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello'));
 
 app.listen(port, () => console.log(`Listening on server ${port}`));
